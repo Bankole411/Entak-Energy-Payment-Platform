666a53889d9f32e0232b9878b647d3ad2e0b1226
Bankole Oluwatobiloba <66980300+Bankole411@users.noreply.github.com>
2022-09-28 17:13:54 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const form= document.getElementById("PayForm");
+form.addEventListener("submit",paynow);
+
+function payNow(e){
+    //Prevent Normal form submittion
+    e.eventDefault();
+
+    //Set configurations
+}
